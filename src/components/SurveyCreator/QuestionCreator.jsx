a5877d5a8108726f095012eef41724448a616781
Siddharth-Nema <siddharthnema2308@gmail.com>
2021-04-03 23:30:21 +0530

    Survey Creation Functionality Completed

--- src/components/SurveyCreator/QuestionCreator.jsx
+++ src/components/SurveyCreator/QuestionCreator.jsx
@@ -1,44 +1,59 @@
 import { useState } from "react";
 
-function QuestionCreator() {
+function QuestionCreator(props) {
+  var [noOfOptions, setNoOfOptions] = useState(1);
 
-    var [title, setTitle] = useState('');
-    var [noOfOptions, setNoOfOptions] = useState(1);
+  function handleTitleChange(event) {
+    props.question.title = event.target.value;
+    console.log(props.question.title);
+  }
 
-    function handleTitleChange(event) {
-        setTitle(event.target.value);
+  function handleChangeInNoOfOptions(event) {
+    setNoOfOptions(event.target.value);
+  }
+
+  function getOptionCreators(number) {
+    var optionsList = [];
+    for (var i = 0; i < number; i++) {
+      optionsList.push(
+        <input
+          id={i}
+          key={i}
+          type="text"
+          className="optionField"
+          onChange={handleOptionChange}
+        />
+      );
     }
+    return optionsList;
+  }
 
-    function handleChangeInNoOfOptions(event) {
-        setNoOfOptions(event.target.value);
-    }
+  function handleOptionChange(event) {
+    props.question.options[event.target.id] = event.target.value;
+  }
 
-    function getOptionCreators(number) {
-        var optionsList = [];
-        for (var i = 0; i < number; i++) {
-            optionsList.push(
-                <input type="text" class='optionField' />
-            )
-        }
-        return optionsList;
-    }
+  return (
+    <div className="QuestionCreator">
+      <form>
+        <label htmlFor="title">Title</label>
+        <input type="text" onChange={handleTitleChange} />
 
+        <select
+          name="noOfOptions"
+          id="noOfOption"
+          value={noOfOptions}
+          onChange={handleChangeInNoOfOptions}
+        >
+          <option value="1">1</option>
+          <option value="2">2</option>
+          <option value="3">3</option>
+          <option value="4">4</option>
+        </select>
 
-    return <div className='QuestionCreator'>
-        <form>
-            <label htmlFor="title">Title</label>
-            <input type="text" class='titleField' id="title" value={title} onChange={handleTitleChange} />
-
-            <select name="noOfOptions" id="noOfOption" value={noOfOptions} onChange={handleChangeInNoOfOptions}>
-                <option value="1">1</option>
-                <option value="2">2</option>
-                <option value="3">3</option>
-                <option value="4">4</option>
-            </select>
-
-            {getOptionCreators(noOfOptions)}
-        </form>
+        {getOptionCreators(noOfOptions)}
+      </form>
     </div>
+  );
 }
 
 export default QuestionCreator;
--- src/pages/SurveyCreator.jsx
+++ src/pages/SurveyCreator.jsx
@@ -1,41 +1,42 @@
-import React, { useState } from 'react';
-import QuestionCreator from '../components/SurveyCreator/QuestionCreator';
-import '../css/SurveyCreator.css';
+import React, { useState } from "react";
+import { useHistory } from "react-router";
+import QuestionCreator from "../components/SurveyCreator/QuestionCreator";
+import "../css/SurveyCreator.css";
 
 function SurveyCreator() {
+  var history = useHistory();
+  var [questionList, setQuestionListN] = useState([]);
 
-    var [questionList, setQuestionList] = useState([]);
+  function addQuestionCreator() {
+    console.log("add Question");
+    setQuestionListN([
+      ...questionList,
+      {
+        title: "",
+        options: [],
+      },
+    ]);
+  }
 
-    function addQuestionCreator() {
-        console.log('add Question');
-        setQuestionList([...questionList, <QuestionCreator />])
-    }
+  function createSurvey() {
+    history.push({
+      pathname: "/dashboard",
+      state: questionList,
+    });
+  }
 
-    function createSurvey() {
-        var createdQuestions = [];
-
-        let questionCreatorsList = document.getElementsByTagName('form');
-        for (let i = 0; i < questionCreatorsList.length; i++) {
-            var question = {};
-            let questionFields = questionCreatorsList[i].children;
-            for (let j = 0; j < questionFields.length; j++) {
-                if (questionFields[j].tagName == 'INPUT') {
-                    let fieldTitle = questionFields[j].classList[0];
-                    question[fieldTitle] = questionFields[j].value;
-                }
-            }
-            createdQuestions.push(question);
-        }
-
-        console.log(createdQuestions);
-    }
-
-    return <div className='SurveyCreator'>
-        <h1>You are at Survey Creator page</h1>
-        {questionList}
-        <button className='addQuestionCreatorButton' onClick={addQuestionCreator}>+</button>
-        <button onClick={createSurvey}>Submit</button>
+  return (
+    <div className="SurveyCreator">
+      <h1>You are at Survey Creator page</h1>
+      {questionList.map((question, index) => (
+        <QuestionCreator key={index} question={question} />
+      ))}
+      <button className="addQuestionCreatorButton" onClick={addQuestionCreator}>
+        +
+      </button>
+      <button onClick={createSurvey}>Submit</button>
     </div>
+  );
 }
 
 export default SurveyCreator;
